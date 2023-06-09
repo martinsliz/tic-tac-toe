69ef871dc5059de68df97df086157407c6f2203d
Liz Martins <elizabeth.martins5@gmail.com>
2023-06-09 13:43:52 -0400

    cool 80s styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import { useState } from 'react'
 import Score from './components/Score'
 import Board from './components/Board'
+import { isDisabled } from '@testing-library/user-event/dist/utils'
 
 const App = () => {
   const winResults = [
@@ -24,11 +25,14 @@ const App = () => {
   const handleClick = (boxIdx) => {
     const updatedBoard = board.map((value, idx) => {
       if (idx === boxIdx) {
+        console.log('boxIdx ' + boxIdx, 'idx ' + idx)
+        console.log('current player is ' + currentPlayer)
         return currentPlayer ? 'X' : 'O'
       } else {
         return value
       }
     })
+    console.log(updatedBoard)
     setBoard(updatedBoard)
 
     const winner = checkWinner(updatedBoard)
@@ -43,7 +47,6 @@ const App = () => {
         setPoints({ ...points, xPoints })
       }
     }
-    console.log(points)
     setCurrentPlayer(!currentPlayer)
   }
 
@@ -52,9 +55,8 @@ const App = () => {
       const [x, y, z] = winResults[i]
 
       if (board[x] && board[x] === board[y] && board[y] === board[z]) {
-        console.log('board' + board[x])
-        return board[x]
         setGameOver(true)
+        return board[x]
       }
     }
   }
@@ -62,21 +64,19 @@ const App = () => {
   const resetBoard = () => {
     setGameOver(false)
     setBoard(Array(9).fill(null))
-    setPoints({ xPoints: 0, oPoints: 0 })
-    setCurrentPlayer(currentPlayer)
   }
 
   return (
     <div className="App">
       <Score points={points} currentPlayer={currentPlayer} />
-      <Board board={board} onClick={handleClick} />
+      <Board board={board} onClick={gameOver ? isDisabled : handleClick} />
       <div>
         <button
           onClick={() => {
             resetBoard()
           }}
         >
-          START AGAIN
+          PLAY AGAIN
         </button>
       </div>
     </div>
